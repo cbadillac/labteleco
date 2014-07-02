07b76f5e8d2cea723ffc758f6c1c120193c5323f
Cristobal Ramirez <legokris@gmail.com>
2014-07-02 01:39:00 -0400

    nombre acordes a guía

--- PreInforme/parameters.m
+++ PreInforme/parameters.m
@@ -1,22 +1,18 @@
 
 %% Parametros
-Zo = 75;
-Zl = 50;
-Z3 = 0;
+Zi = 75;
+Zo = 50;
 
-%% A resolver
-syms Z1 Z2;
+%% A resolver modelo "L"
+syms Z2 Z3;
+Z1 = 0;
 
 [R1, R2] = solve(...
-                Zl == Z2.*(Z3+Zo) ./(Z2+Z3+Zo) + Z1, ...
-                Zo == Z2.*(Z1+Zl) ./(Z1+Z2+Zl) + Z3, ...
-                Z1, Z2);
+                Zi == Z2.*(Z3+Zo) ./(Z2+Z3+Zo) + Z1, ...
+                Zo == Z2.*(Z1+Zi) ./(Z1+Z2+Zi) + Z3, ...
+                Z2, Z3);
 
 R1
 R2
-
-%% My System
-%R2 = solve(Zo - Z2.*(Zo-Zl) == (Z2.*(Zl-Zo) -Zo.*Zl)/(Zo-Z2));
-%R1 = Zo - R2/(Zo -Zl);
 
 
